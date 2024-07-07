6dded39ba3e87df3b9147a1db79d1407b440a45f
DevShashankSharma <editorshashank429@gmail.com>
2024-07-07 13:00:07 +0530

    operators

---
+++ chapter 2/operators.py
@@ -0,0 +1,29 @@
+
+#! Arithmetic Operators
+a = 9
+b = 5
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+
+
+#! Comparison Operators 
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
+
+
+#! Logical Operators 
+a = True
+b = False
+print("a and b :", a and b)
+print("a or b :", a or b)
+print("not a :", not a)
