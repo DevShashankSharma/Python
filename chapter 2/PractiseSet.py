c39bac04c1ac9ca96b274d831728d88192ac6059
DevShashankSharma <editorshashank429@gmail.com>
2024-07-07 13:30:25 +0530

    ch2 practiceset

---
+++ chapter 2/PractiseSet.py
@@ -0,0 +1,9 @@
+# chapter 2\PractiseSet.py
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("The sum of {0} and {1} is {2}".format(a, b, a + b))
+print("The remainder of {0} and {1} is {2}".format(a, b, a % b))
+print("The comparison between {0} and {1} is {2}".format(a, b, a > b))
+print("The average of {0} and {1} is {2}".format(a, b, (a + b) / 2))
+print("The square of {0} is {1}".format(a, a ** 2))
