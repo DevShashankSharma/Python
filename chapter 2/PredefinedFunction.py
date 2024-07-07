82b8e8454516905c38dba44c8c262b524bd0a2a9
DevShashankSharma <editorshashank429@gmail.com>
2024-07-07 13:14:05 +0530

    Input , Type , Typecasting function

---
+++ chapter 2/PredefinedFunction.py
@@ -0,0 +1,24 @@
+
+#! Type function
+
+a = "10"
+b = 20
+
+print(type(a))
+print(type(b))
+
+#! Typecasting
+
+a = int(a)
+b = float(b)
+
+print(type(a))
+print(type(b))
+
+
+#! Input function
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+print(a + b)
