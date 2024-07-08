194e2c06017bb551d0292b86ef464f7d59b41aa5
DevShashankSharma <editorshashank429@gmail.com>
2024-07-08 09:46:24 +0530

    List and tuple

---
+++ chapter 4/main.py
@@ -0,0 +1,36 @@
+
+# #! Lists
+
+# a = [2,"hello",3.4]
+
+# print(a[1])
+
+# a[1] = "Hi"
+
+# print(a[1])
+
+# # List Slicing
+
+# print(a[0:2])
+
+# print(a.sort())
+
+
+# a = [1,2,3,4,5,6,7,8,9,0,1,10]
+
+# print(a[0:9:2])
+# a.sort()
+# print(a)
+
+# a.remove(0)
+
+
+#! tuple 
+a = (1,2,3,4,5,6,7,8,9,0,1,10,'hello',3.4,True,False)
+
+print(a[0:9:2])
+# a.sort()
+print(a)
+
+# tuple method  
+a.remove(0)
---
+++ chapter 4/practiceSet.py
@@ -0,0 +1,7 @@
+l = [3,2,8,2]
+
+print(sum(l))
+
+a = (7, 0, 8, 0, 0, 9)
+n = a.count(0)
+print(n)
